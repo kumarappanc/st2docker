3d7b95f14e94bd159a10552c5e812f567d4f2148
kumar chockalingam <kumarappanc@gmail.com>
2016-05-20 13:10:07 +1000

    fix

--- start.sh
+++ start.sh
@@ -23,13 +23,13 @@ cp -a -r -n /tmp/st2docker/packs/* /opt/stackstorm/packs
 /usr/bin/st2ctl reload
 echo $1
 echo $2
-user_list= $1
-username = $2
+user_list= "$1"
+username= "$2"
 
 if [ -n "$user_list" ]
 then
   python /tmp/st2docker/add_user.py ${user_list}
-  password=`python /tmp/st2docker/get_user_password.py ${username} ${user_list}
+  password=`python /tmp/st2docker/get_user_password.py ${username} ${user_list}`
   TOKEN=`st2 auth devops -p ${password} | grep token | awk -F'|' '{print $3}'`
   for pack in "/tmp/st2contrib/packs/"*
   do
